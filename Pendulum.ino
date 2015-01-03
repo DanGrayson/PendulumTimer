9c9a515e07faeb77fd3461cb67b22f6b5e79f6ea
Daniel R. Grayson <dan@math.uiuc.edu>
2015-01-03 11:07:58 -0800

    keep track of counter overflow

--- Pendulum.ino
+++ Pendulum.ino
@@ -16,7 +16,8 @@ void setup() {
 
   // See table 14-6.  Here we set the timer source to come from the I/O clock with prescaling by division by 8, which
   // makes it tick at 1/8 of the CPU clock rate of 16 Mhz, i.e., at 2 Mhz.  It would overflow after 2^15 microseconds,
-  // which is about 32 milliseconds.  
+  // which is about 32 milliseconds, or 30 times per second.
+#define DIVIDER 8
   bitWrite(TCCR1B,CS12,0);
   bitWrite(TCCR1B,CS11,1);
   bitWrite(TCCR1B,CS10,0);
@@ -24,21 +25,20 @@ void setup() {
 }
 
 void loop() {
-  int level = 1024-analogRead(A0);
-  static unsigned int c, oldc;
-  oldc = c;
-  cli(); c = TCNT1; sei();
-  static unsigned long oldtime, time;
-  oldtime = time;
-  time = millis();
-  lcd.clear();
-  lcd.print(level);
-  // if (Serial) lcd.print(" S");
-  lcd.gotoXY(0, 1);
-  lcd.print(c);
-  // if (Serial)			// this conversion has a 10msec delay!
-    // This printing is surprisingly slow if the serial port
-    // isn't connected; that's why we test first.
-    Serial.print(level), Serial.print(' '), Serial.print(c), Serial.print(' '), Serial.print(1000 * (double) (c-oldc) / (double)  (time-oldtime)), Serial.print('\r'), Serial.print('\n');
-  delay(27);			// if the delay is too long, the counter may overflow; here we set it not quite so large
+  while (1) {
+    char buf[20];
+    int level = 1024-analogRead(A0);
+    static unsigned tc1;
+    tc1 = TCNT1;
+    static unsigned long ticks;
+    unsigned diff = tc1 - (int)ticks;
+    ticks += diff;
+    static unsigned long hsec;
+    unsigned long last_hsec = hsec;
+    hsec = ticks/(F_CPU/DIVIDER/10); // elapsed tenths of seconds
+    if (hsec/2 != last_hsec/2) {    // display 5 times per second
+      lcd.gotoXY(0,0), sprintf(buf,"%08lu",hsec), lcd.print(buf);
+      lcd.gotoXY(0,1), sprintf(buf,"%03d %4u",level,diff/2), lcd.print(buf);
+    }
+  }
 }
