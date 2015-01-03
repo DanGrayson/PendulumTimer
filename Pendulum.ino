8bfc051aae9b90b33fde6e7fc4e2557114743eeb
Daniel R. Grayson <dan@math.uiuc.edu>
2015-01-02 16:01:08 -0800

    start accessing Timer-Counter 1

--- Pendulum.ino
+++ Pendulum.ino
@@ -1,13 +1,30 @@
 #include <AStar32U4Prime.h>
 
+  // Here we start experimenting with Timer-Counter 1, which is
+  // the one whose values can be captured when the analog
+  // comparator triggers.
+
+static AStar32U4PrimeLCD lcd;
+
 void setup() {
+  // See table 14-5 on page 123 in the datasheet.  Normally WGM1
+  // is initialized to 0001 by the library.  Here we initialize
+  // it to 0000 to get the full 16 bit count, instead of just 8.
+  TCCR1B &= ~ (1 << WGM13);
+  TCCR1B &= ~ (1 << WGM12);
+  TCCR1A &= ~ (1 << WGM11);
+  TCCR1A &= ~ (1 << WGM10);
 }
 
 void loop() {
   int level = 1024-analogRead(A0);
-  static AStar32U4PrimeLCD lcd;
+  static unsigned int c, oldc;
+  oldc = c;
+  cli(); c = TCNT1; sei();
   lcd.clear();
   lcd.print(level);
-  Serial.print(level), Serial.print(' ');
+  lcd.gotoXY(0, 1);
+  lcd.print(c);
+  Serial.print(level), Serial.print('@'), Serial.print(c), Serial.print('\r'), Serial.print('\n');
   delay(100);
 }
