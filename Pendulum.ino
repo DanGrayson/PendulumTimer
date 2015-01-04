c65437a7316e0dae6169b6e4ac97b6116eb31730
Daniel R. Grayson <dan@math.uiuc.edu>
2015-01-03 16:34:26 -0800

    add time capture

--- Pendulum.ino
+++ Pendulum.ino
@@ -6,6 +6,10 @@
 
 static AStar32U4PrimeLCD lcd;
 
+#define DISPLAY_FREQ 5		// per second
+#define DISPLAY_RESOLUTION 100	// per second
+#define TRIGGER_LEVEL 500	// 0..1023, 0 is white, 1023 is black.
+
 void setup() {
   // See table 14-5 on page 123 in the datasheet.  Normally WGM1 is initialized to 0001 by the
   // library.  Here we initialize it to 0000 to get the full 16 bit count, instead of just 8.
@@ -17,31 +21,52 @@ void setup() {
   // See table 14-6.  Here we set the timer source to come from the I/O clock with prescaling by
   // division by 8, which makes it tick at 1/8 of the CPU clock rate of 16 Mhz, i.e., at 2 Mhz.  It
   // would overflow after 2^15 microseconds, which is about 32 milliseconds, or 30 times per second.
-#define TIMER1_FREQ (F_CPU/8)
+# define TICK_FREQ (F_CPU/8)
   bitWrite(TCCR1B,CS12,0);
   bitWrite(TCCR1B,CS11,1);
   bitWrite(TCCR1B,CS10,0);
 
-}
+  // Set up ADC MUX.  This will break analogRead().
+  ADMUX = 0,		// ADC0 from the ADC MUX.
+    ADCSRA = (1<<ADATE),	// ADC Auto Trigger Enable
+    ADCSRB = (1<<ACME)		// Analog Comparator Multiplexer Enable, free running mode
+    ;
 
-#define DISPLAY_FREQ 5		// per second
-#define DISPLAY_RESOLUTION 100	// per second
+  // Set up analog comparator:
+  ACSR = (1<<ACIC)		// Input Capture Enable
+    | (1<<ACBG)			// Analog Comparator Bandgap Select (pin AIN0 is already in use on
+				// the A*')
+    | (1<<ACI)			// clear the Analog Comparator Interrupt Flag
+    | ((1<<ACIS0)|(1<<ACIS1))	// Comparator Interrupt on Rising Output Edge
+    ;
+
+}
 
 void loop() {
   while (1) {
-    char buf[20];
-    int level = 1024-analogRead(A0);
-    static unsigned tc1;
-    tc1 = TCNT1;
-    static unsigned long ticks;
-    unsigned diff = tc1 - (int)ticks;
-    ticks += diff;
-    static unsigned long display;
-    unsigned long last_display = display;
-    display = ticks/(TIMER1_FREQ/DISPLAY_FREQ);
-    if (display != last_display) {
-      lcd.gotoXY(0,0), sprintf(buf,"%08lu",ticks/(TIMER1_FREQ/DISPLAY_RESOLUTION)), lcd.print(buf);
-      lcd.gotoXY(0,1), sprintf(buf,"%03d %4u",level,diff), lcd.print(buf);
+    static unsigned long tick;
+    unsigned diff = TCNT1 - (unsigned)tick;
+    tick += diff;
+    static unsigned long last_display_tick;
+    static unsigned aci_counter;
+    static unsigned long last_aci_tick;
+    if (ACSR & (1<<ACI)) {
+      aci_counter++;
+      bitSet(ACSR,ACI);
+      unsigned int input_capture = ICR1;
+      last_aci_tick = input_capture <= (unsigned) tick
+	? (((tick >> 16)  ) << 16) | input_capture
+	: (((tick >> 16)-1) << 16) | input_capture;
+    }
+    if (tick - last_display_tick > TICK_FREQ/DISPLAY_FREQ) {
+      last_display_tick = tick;
+      char buf[20];
+      lcd.gotoXY(0,0),
+	sprintf(buf,"%8lu",tick/(TICK_FREQ/DISPLAY_RESOLUTION)),
+	lcd.print(buf);
+      lcd.gotoXY(0,1),
+	sprintf(buf,"%2u %5lu",aci_counter,last_aci_tick/(TICK_FREQ/DISPLAY_RESOLUTION)),
+	lcd.print(buf);
     }
   }
 }
