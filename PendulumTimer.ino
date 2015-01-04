f8b3c240672b34089a3be51cec586c100d1bd991
Daniel R. Grayson <dan@math.uiuc.edu>
2015-01-04 09:48:37 -0800

    clean up

--- PendulumTimer.ino
+++ PendulumTimer.ino
@@ -1,5 +1,12 @@
 #include <AStar32U4Prime.h>
 
+#define bitCopy(word,from,to) (bitRead(word,from) << to)
+#define bitCopy2(word,from1,to1,from2,to2) ((bitRead(word,from1) << to1) | \
+					    (bitRead(word,from2) << to2))
+#define bitCopy3(word,from1,to1,from2,to2,from3,to3) ((bitRead(word,from1) << to1) | \
+						      (bitRead(word,from2) << to2) | \
+						      (bitRead(word,from3) << to3))
+
 #define DISPLAY_FREQ 5		// per second
 #define DISPLAY_RESOLUTION 100	// per second
 #define TRIGGER_LEVEL 500	// 0..1023, 0 is white, 1023 is black.
@@ -8,7 +15,7 @@
   // Here we set the timer source to come from the I/O clock with prescaling by
   // division by 8, which makes it tick at 1/8 of the CPU clock rate of 16 Mhz,
   // i.e., at 2 Mhz.  It would overflow after 2^15 microseconds, which is about
-  // 32 milliseconds, or 30 times per second.
+  // 33 milliseconds, or 30 times per second.
 
 #define TICK_FREQ (F_CPU/DIVISOR)
 
@@ -25,35 +32,28 @@
 #  define CLOCK_SELECT 5
 #endif
 
-#define WGM1 0b0000
+// Waveform Generation Mode;
   // See table 14-5 on page 123 in the datasheet.  Normally WGM1 is initialized to 0001 by the
   // library.  Here we initialize it to 0000 to get the full 16 bit count, instead of just 8.
-
-#define bitCopy(word,from,to) (bitRead(word,from) << to)
-#define bitCopy2(word,from1,to1,from2,to2) ((bitRead(word,from1) << to1) | \
-					    (bitRead(word,from2) << to2))
-#define bitCopy3(word,from1,to1,from2,to2,from3,to3) ((bitRead(word,from1) << to1) | \
-						      (bitRead(word,from2) << to2) | \
-						      (bitRead(word,from3) << to3))
-#define bit(to) (1 << to)
+#define WGM1 0b0000
 
 void setup() {
-  TCCR1B = bitCopy2(WGM1,3,WGM13,2,WGM12) | bitCopy3(CLOCK_SELECT,2,CS12,1,CS11,0,CS10);
-  TCCR1A = bitCopy2(WGM1,1,WGM11,0,WGM10);
-
-  // Set up ADC MUX.  This will break analogRead().
-  ADMUX = 0;			// ADC0 from the ADC MUX.
-  ADCSRA = bit(ADATE);		// ADC Auto Trigger Enable
-  ADCSRB = bit(ACME);		// Analog Comparator Multiplexer Enable, free running mode
-
-  // Set up analog comparator:
-  ACSR =
-      bit(ACIC)			// Input Capture Enable
-    | bit(ACBG)			// Analog Comparator Bandgap Select (pin AIN0 is already in use on the A*')
-    | bit(ACI)			// clear the Analog Comparator Interrupt Flag
-    | bit(ACIS1) | bit(ACIS0)	// Comparator Interrupt on Rising Output Edge
-    ;
-
+  TCCR1A =			// Timer/Counter1 Control Register A
+    bitCopy2(WGM1,1,WGM11,0,WGM10);
+  TCCR1B =			// Timer/Counter1 Control Register B
+    bitCopy2(WGM1,3,WGM13,2,WGM12) |
+    bitCopy3(CLOCK_SELECT,2,CS12,1,CS11,0,CS10) ;
+  ADMUX =			// ADC Multiplexer Selection Register
+    0;				//   ADC0 from the ADC MUX.  This will break analogRead().
+  ADCSRA =			// ADC Control and Status Register A
+    bit(ADATE);			//   ADC Auto Trigger Enable
+  ADCSRB =			// ADC Control and Status Register B
+    bit(ACME);	                //   Analog Comparator Multiplexer Enable, free running mode
+  ACSR =			// Analog Comparator Control and Status Register
+      bit(ACIC)			//   Input Capture Enable
+    | bit(ACBG)			//   Analog Comparator Bandgap Select (pin AIN0 is already in use on the A*')
+    | bit(ACI)			//   clear the Analog Comparator Interrupt Flag
+    | bit(ACIS1) | bit(ACIS0);	//   Comparator Interrupt on Rising Output Edge
 }
 
 #define TICKS_PER_MS (TICK_FREQ/1000)
