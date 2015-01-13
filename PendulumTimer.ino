3f78e3cce0c6dc43efc1dce3d497e63acf561d28
Daniel R. Grayson <dan@math.uiuc.edu>
2015-01-13 16:13:59 -0600

    improve algorithm, start recording sum of squares

--- PendulumTimer.ino
+++ PendulumTimer.ino
@@ -1,33 +1,58 @@
+// Plug the sensor into pin A5
+
+// The pendulum swings back and forth, and we assume the sensor detects it twice per
+// cycle, so we expect the even ticks to be evenly spaced and we expect the odd ticks to
+// be evenly spaced.  Since the sensor is not in the exact center, we don't expect all
+// the ticks to be evenly spaced.
+
 #include <AStar32U4Prime.h>
 
 #define TRUE 1
 #define FALSE 0
 
+#define DIVISOR 8
+  // Here we set the timer source to come from the I/O clock with prescaling by
+  // division by 8, which makes it tick at 1/8 of the CPU clock rate of 16 Mhz,
+  // i.e., at 2 Mhz.  It would overflow after 2^15 microseconds, which is about
+  // 33 milliseconds, or 30 times per second.
+#define TIMER_FREQ (F_CPU/DIVISOR)
+#define SECOND TIMER_FREQ
+#define MILLISECOND (SECOND/1000)
+#define MINUTES_PER_HOUR 60
+#define SECONDS_PER_MINUTE 60
+#define MINUTE (SECONDS_PER_MINUTE*SECOND)
+#define HOUR (MINUTES_PER_HOUR*(uint64_t)MINUTE)
+
+#define rounded_quotient(x,y) ((x+y/2)/y)
+
 // start user configuration section
-#define REMAP_RS_TO_PIN 22		// LCD:RS is usually on pin 7
-#define TICK_PERIOD 385			// my Ansonia mantle clock has 384.615 ms between ticks
-#define REFRACTORY_PERIOD (TICK_PERIOD-100)	// milliseconds after each tick to ignore new events
-#define RESTART_PERIOD    (TICK_PERIOD+100)	// milliseconds after which to declare a missed tick, so restart the count
-#define USE_BANDGAP_REF FALSE		// use the bandgap reference (1.2V) for the comparator, instead of pin AIN0 (which requires RS to be remapped)
+#define USE_BANDGAP_REF FALSE // use the bandgap reference (1.2V) for the comparator, instead of pin AIN0 (which requires RS to be remapped)
+#define LCD_RS_PIN 22		// remap LCD:RS to pin 22
+#define TICKS_PER_MINUTE 156    // my Ansonia mantle clock has 156 ticks per minute, but my tall case clock has 64
 // end user configuration section
 
+#ifdef TICKS_PER_MINUTE
+#define TICK_PERIOD rounded_quotient(MINUTE,TICKS_PER_MINUTE)
+#else
+#define TICK_PERIOD rounded_quotient(HOUR,TICKS_PER_HOUR)
+#endif
+#define TICKS_PER_CYCLE 2
+#define CYCLE (TICKS_PER_CYCLE*TICK_PERIOD)
+#define TOLERANCE (100*MILLISECOND) // milliseconds; restart the count if the tolerance is not met
+
 // To remap LCD:RS to another pin requires cutting a surface mount jumper on the board and soldering a wire into the RS hole near the LCD connector.
+#ifndef LCD_RS_PIN
+#define LCD_RS_PIN 7
+#endif
 
 // Improvements needed:
 //   determine REFRACTORY_PERIOD and RESTART_PERIOD dynamically
 
-#define MAX_DISPLAY_FREQ 5		// per second
 #define DISPLAY_RESOLUTION 100		// per second
 
 class AStar32U4PrimeLCD_remapped : public PololuHD44780Base
 {
-  static const uint8_t rs =
-#ifdef REMAP_RS_TO_PIN
-    REMAP_RS_TO_PIN
-#else
-    7
-#endif    
-    , e = 8, db4 = 14, db5 = 17, db6 = 13, db7 = IO_D5;
+  static const uint8_t rs = LCD_RS_PIN, e = 8, db4 = 14, db5 = 17, db6 = 13, db7 = IO_D5;
   void sendNibble(uint8_t data) {
     FastGPIO::Pin<db4>::setOutput(data >> 0 & 1);
     FastGPIO::Pin<db5>::setOutput(data >> 1 & 1);
@@ -64,14 +89,6 @@ static AStar32U4PrimeLCD_remapped lcd;
 						      (bitRead(word,from2) << to2) | \
 						      (bitRead(word,from3) << to3))
 
-#define DIVISOR 8
-  // Here we set the timer source to come from the I/O clock with prescaling by
-  // division by 8, which makes it tick at 1/8 of the CPU clock rate of 16 Mhz,
-  // i.e., at 2 Mhz.  It would overflow after 2^15 microseconds, which is about
-  // 33 milliseconds, or 30 times per second.
-
-#define TIMER_FREQ (F_CPU/DIVISOR)
-
 // see Datasheet, table 13-9 or table 14-6, Clock Select Bit Description
 #if   DIVISOR == 1
 #  define CLOCK_SELECT 1
@@ -90,12 +107,16 @@ static AStar32U4PrimeLCD_remapped lcd;
   // library.  Here we initialize it to 0000 to get the full 16 bit count, instead of just 8.
 #define WGM1 0b0000
 
+typedef uint64_t counter_t;
+static counter_t timer = 1L << 16; // the timer, continuously updated
+static void update_timer() { timer += (uint16_t)TCNT1 - (uint16_t)timer; }
 static void timer1_setup() {
   TCCR1A =			// Timer/Counter1 Control Register A
     bitCopy2(WGM1,1,WGM11,0,WGM10);
   TCCR1B =			// Timer/Counter1 Control Register B
     bitCopy2(WGM1,3,WGM13,2,WGM12) |
     bitCopy3(CLOCK_SELECT,2,CS12,1,CS11,0,CS10) ;
+  update_timer();
 }
 
 static void adc_setup() {
@@ -129,80 +150,65 @@ void setup() {
 int analogueRead(uint8_t pin) {
   ADCSRA =				   // Analog Comparator Control and Status Register
     bit(ADEN)				   //   ADC Enable
-    | bit(ADPS2)|bit(ADPS1)|bit(ADPS0);	   //   ADC Prescaler Select Bits (divide clock by 128)
+    | bit(ADPS2)|bit(ADPS1)|bit(ADPS0);	   //   ADC Prescaler Select Bits (divide clock by 128), see init()
   int n = analogRead(pin);
   adc_setup();
   return n;
 }
 
-#define TICKS_PER_MS (TIMER_FREQ/1000)
-#define TICKS_PER_HOUR (60L*60L*TIMER_FREQ)
-
-typedef uint64_t counter_t;
+#define PREC 10
+#define SKIP_TICKS 2		// Ignoring the first event is a good idea, since it may not correspond to the
+				// leading edge of the pendulum rod.  Ignoring one other event seems also to help.
 
 void loop() {
-  static bool display_needed = TRUE;
   static uint16_t reset_counter;
-  static uint32_t tick_counter;
-  static counter_t timer = TCNT1, // initialize the timer so the refractory period is in effect at the start
-    first_tick_time, second_tick_time,
-    previous_tick_time, previous_odd_tick_time, previous_even_tick_time;
+  uint32_t tick_counter = 0, cycle_counter = 0;
+  counter_t
+    cycle_sum = 0,		    // sum of the cycle timings (a cycle is two ticks, or one complete pendulum cycle)
+    cycle_square_sum = 0,	    // sum of the squares of the cycle timings, shifted right by PREC bits
+    last_tick_time[2] = {0,0};	    // last_tick_time[0] is the previous tick time, and
+				    // last_tick_time[1] is the one before that
+  char buf[20];
+  lcd.gotoXY(0,0), lcd.print("waiting ");
+  lcd.gotoXY(0,1), sprintf(buf,"reset %2u",reset_counter), lcd.print(buf);
+  bitSet(ACSR,ACI);		// clear comparator event flag initially, to ignore some possible noise
   while (1) {
-    timer += (uint16_t)TCNT1 - (uint16_t)timer;
+    update_timer();
     if (ACSR & bit(ACI)) {
-      bitSet(ACSR,ACI);
+      bitSet(ACSR,ACI);		// clear comparator event flag
       uint16_t input_capture = ICR1;
-      counter_t this_tick_time = input_capture <= (uint16_t) timer
-	? (((timer >> 16)  ) << 16) | input_capture
-	: (((timer >> 16)-1) << 16) | input_capture;
-      counter_t diff_timer = this_tick_time - previous_tick_time;
-      if (diff_timer > RESTART_PERIOD * TICKS_PER_MS) {
-	reset_counter++;
-	display_needed = TRUE;
-	tick_counter =
-	  first_tick_time = previous_tick_time =
-	  previous_odd_tick_time = previous_even_tick_time = 0;
-      }
-      if (diff_timer > REFRACTORY_PERIOD * TICKS_PER_MS) {
+      counter_t this_tick_time = ((input_capture <= (uint16_t) timer ? (timer >> 16) : ((timer >> 16) - 1)) << 16) | input_capture;
+      if (tick_counter == 0) {
 	tick_counter++;
-	display_needed = TRUE;
-	previous_tick_time = this_tick_time;
-	if (tick_counter == 1) first_tick_time = this_tick_time;
-	else if (tick_counter == 2) second_tick_time = this_tick_time;
-	if (tick_counter & 1) previous_odd_tick_time = this_tick_time;
-	else previous_even_tick_time = this_tick_time;
+	last_tick_time[0] = this_tick_time;
       }
-    }
-    static counter_t previous_display_time;
-    if (display_needed && timer - previous_display_time > TIMER_FREQ/MAX_DISPLAY_FREQ) {
-      display_needed = FALSE;
-      previous_display_time = timer;
-      uint32_t tick_rate = // in ticks per hundred hours
-	tick_counter >= 4
-	? 60.			// minutes per hour
-	* 60.			// seconds per minute
-	* TIMER_FREQ		// timer counts per second
-	* 100.			// number of hours
-	/ (
-	   // The pendulum swings back and forth, and we assume the sensor detects it twice per
-	   // cycle, so we expect the even ticks to be evenly spaced and we expect the odd ticks to
-	   // be evenly spaced.  Since the sensor is not in the exact center, we don't expect all
-	   // the ticks to be evenly spaced.  Here we average the average spacing between even ticks
-	   // with the average spacing between odd ticks.  This formula starts working with the 4th
-	   // tick.
-	   ((float)(previous_odd_tick_time-first_tick_time)/((tick_counter-1)/2) +
-	    (float)(previous_even_tick_time-second_tick_time)/((tick_counter-2)/2))/2
-	   / 2			// the spacing between odd ticks (or even ticks) is twice the tick rate, so divide by 2
-	   )
-	: 0.;
-      char buf[20];
-      lcd.gotoXY(0,0),
-	sprintf(buf,"%2u:%5lu",reset_counter,tick_counter),
-	lcd.print(buf);
-      lcd.gotoXY(0,1),
-	// sprintf(buf,"%4u",analogueRead(A5));
-	sprintf(buf,"%5lu.%02lu",tick_rate/100,tick_rate%100), // print the rate in the form of ticks per hour, as a decimal fraction
-	lcd.print(buf);
+      else {
+	counter_t this_tick_length = this_tick_time - last_tick_time[0];
+	if (this_tick_length > 250 * MILLISECOND) { // allow entire pendulum rod to pass by, and ignore noisy nearby events
+	  tick_counter++;
+	  if (tick_counter > 2 + SKIP_TICKS) {
+	    counter_t this_cycle_length = this_tick_time - last_tick_time[1];
+	    if (this_cycle_length < CYCLE - TOLERANCE || this_cycle_length > CYCLE + TOLERANCE) break;
+	    cycle_counter++;
+	    cycle_sum += this_cycle_length;
+	    cycle_square_sum += (this_cycle_length * this_cycle_length) >> PREC;
+	  }
+	  last_tick_time[1] = last_tick_time[0];
+	  last_tick_time[0] = this_tick_time;
+	  uint32_t tick_rate =	// ticks per hundred hours
+	    cycle_counter > 0
+	    ? (100 * HOUR * TICKS_PER_CYCLE) / (rounded_quotient(cycle_sum,cycle_counter))
+	    : 0;
+	  lcd.gotoXY(0,0),
+	    sprintf(buf,"%2u:%5lu",reset_counter,tick_counter),
+	    lcd.print(buf);
+	  lcd.gotoXY(0,1),
+	    // sprintf(buf,"%4u",analogueRead(A5));
+	    sprintf(buf,"%5lu.%02lu",tick_rate/100,tick_rate%100), // ticks per hour
+	    lcd.print(buf);
+	}
+      }
     }
   }
+  reset_counter++;
 }
